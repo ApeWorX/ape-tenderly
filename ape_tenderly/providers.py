b6a60a3ad2c34d191658a36e99dded64fa51736f
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2022-04-25 18:07:38 +0200

    fix: bypass fix so that tenderly works properly

--- ape_tenderly/providers.py
+++ ape_tenderly/providers.py
@@ -2,6 +2,7 @@ import os
 
 import requests  # type: ignore
 from ape.api import ProviderAPI, Web3Provider
+from ape.exceptions import ConfigError
 from ape.utils import cached_property
 from web3 import HTTPProvider, Web3
 
@@ -13,14 +14,16 @@ class TenderlyProvider(Web3Provider, ProviderAPI):
             return os.environ["TENDERLY_FORK_ID"]
 
         elif "TENDERLY_FORK_SERVICE_URI" in os.environ:
+            fork_network_name = self.network.name.replace("-fork", "")
+            chain_id = self.network.ecosystem.get_network(fork_network_name).chain_id
             response = requests.post(
                 os.environ["TENDERLY_FORK_SERVICE_URI"],
-                json={"network_id": str(self.network.chain_id)},
+                json={"network_id": str(chain_id)},
             )
             return response.json()["simulation_fork"]["id"]
 
         else:
-            raise
+            raise ConfigError("No valid tenderly fork id found.")
 
     @property
     def uri(self) -> str:
