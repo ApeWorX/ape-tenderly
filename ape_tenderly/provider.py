9edbb4cefba01d2e48805daf0c129b9ef7a7581c
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2026-09-24 22:28:28 -0400

    chore: fix ruff/mdformat/mypy/deps for CI

--- ape_tenderly/provider.py
+++ ape_tenderly/provider.py
@@ -11,7 +11,9 @@ from web3.gas_strategies.rpc import rpc_gas_price_strategy
 try:
     from web3.middleware import ExtraDataToPOAMiddleware  # type: ignore
 except ImportError:
-    from web3.middleware import geth_poa_middleware as ExtraDataToPOAMiddleware  # type: ignore
+    from web3.middleware import (
+        geth_poa_middleware as ExtraDataToPOAMiddleware,  # type: ignore  # noqa: N812
+    )
 
 from .client import Fork, TenderlyClient
 
